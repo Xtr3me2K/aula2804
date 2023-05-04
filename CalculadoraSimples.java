398b0bc6f544f4036fdfc0ed1a712e2f844bed23
Xtr3me2K <130668153+Xtr3me2K@users.noreply.github.com>
2023-05-04 09:09:34 -0300

    Commit do Luisão

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -16,24 +16,29 @@ do{
 
     System.out.println("1. Soma");
     System.out.println("2. Subtração");
-    System.out.println("3. Sair");
+    System.out.println("3. Multiplicação");
+    System.out.println("4. Divisão");
+    System.out.println("5. Sair");
     
     System.out.println("Opção: ");
     op = entrada.nextInt();
 
     if(op == 1){
-        System.out.println(\n\t\t\t -- Soma --\n);
+        System.out.println("\n\t\t\t -- Soma --\n");
     }else if(op == 2){
-    System.out.println(\n\t\t\t -- Subtração -- \n);
-    } else if(op == 3){
+    System.out.println("\n\t\t\t -- Subtração -- \n");
+}else if(op == 3){
+    System.out.println("\n\t\t\t -- Multiplicação -- \n");
+}else if(op == 4){
+    System.out.println("\n\t\t\t -- Divisão -- \n");
+    } else if(op == 5){
         System.out.println("Forte abraço!");
     }else{
         System.out.println("Opção" + op + "Incorreta!");
     }
 
-}while(op!=3);
+}while(op!=5);
 
  }
- //Comentário do pai
  
 }
