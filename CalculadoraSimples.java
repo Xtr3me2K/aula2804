1c8600d370c180c1fb3dfbf2ce25dc0dc55535e9
GILBERTO BRAGA <58963241+gilbertexbom@users.noreply.github.com>
2023-05-04 10:53:35 -0300

    Commite de refatoração de código

--- CalculadoraSimples.java
+++ CalculadoraSimples.java
@@ -2,43 +2,41 @@ import java.util.Scanner;
 
 public class CalculadoraSimples{
 
- public static void main(String[] args) {
-    
+    public static void main(String[] args) {
+        //Declaração de variáveis
+        int n1, n2, total, op;
 
-    //Declaração de variáveis
-    int n1, n2, total, op;
+        //Criação e instância do objeto de entrada
+        Scanner entrada = new Scanner(System.in);
+        do{
+            System.out.println("\n\t\t\t -- Calculadora Simples -- \n");
 
-    //Criação e instância do objeto de entrada
-    Scanner entrada = new Scanner(System.in);
-do{
+            //Menu
+            System.out.println("1. Soma");
+            System.out.println("2. Subtração");
+            System.out.println("3. Multiplicação");
+            System.out.println("4. Divisão");
+            System.out.println("5. Sair");
+            
+            //Opção escolhida no menu
+            System.out.println("Opção: ");
+            op = entrada.nextInt();
 
-    System.out.println("n\t\t\t -- Calculadora Simples -- \n");
+            if(op == 1){
+                System.out.println("\n\t\t\t -- Soma --\n");
+            }else if(op == 2){
+                System.out.println("\n\t\t\t -- Subtração -- \n");
+            }else if(op == 3){
+                System.out.println("\n\t\t\t -- Multiplicação -- \n");
+            }else if(op == 4){
+                System.out.println("\n\t\t\t -- Divisão -- \n");
+            } else if(op == 5){
+                System.out.println("Forte abraço!");
+            }else{
+                System.out.println("Opção " + op + " incorreta!");
+            }
+        }while(op!=5);
 
-    System.out.println("1. Soma");
-    System.out.println("2. Subtração");
-    System.out.println("3. Multiplicação");
-    System.out.println("4. Divisão");
-    System.out.println("5. Sair");
-    
-    System.out.println("Opção: ");
-    op = entrada.nextInt();
-
-    if(op == 1){
-        System.out.println("\n\t\t\t -- Soma --\n");
-    }else if(op == 2){
-    System.out.println("\n\t\t\t -- Subtração -- \n");
-}else if(op == 3){
-    System.out.println("\n\t\t\t -- Multiplicação -- \n");
-}else if(op == 4){
-    System.out.println("\n\t\t\t -- Divisão -- \n");
-    } else if(op == 5){
-        System.out.println("Forte abraço!");
-    }else{
-        System.out.println("Opção" + op + "Incorreta!");
-    }
-
-}while(op!=5);
-
- }
+    } //main
  
-}
+} //classe
